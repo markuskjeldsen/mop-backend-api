e376b75ea99b750389bd48687ede228e5628e47b
markuskjeldsen <kinglink224@gmail.com>
2025-11-25 15:23:02 +0100

    new way of doing the document, this one uses a ready made pdf

--- go.mod
+++ go.mod
@@ -45,6 +45,9 @@ require (
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
 	github.com/oschwald/maxminddb-golang v1.13.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
+	github.com/phpdave11/gofpdf v1.4.3 // indirect
+	github.com/phpdave11/gofpdi v1.0.15 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.10.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.12 // indirect
--- internal/internal.go
+++ internal/internal.go
@@ -15,10 +15,11 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
-	"github.com/go-pdf/fpdf"
 	"github.com/google/uuid"
 	"github.com/markuskjeldsen/mop-backend-api/initializers"
 	"github.com/markuskjeldsen/mop-backend-api/models"
+	fpdf "github.com/phpdave11/gofpdf"
+	"github.com/phpdave11/gofpdf/contrib/gofpdi"
 )
 
 // Example function to update status and log the change
@@ -113,137 +114,51 @@ func pdfwrite(pdf *fpdf.Fpdf, message string) {
 	// pdf.Ln() is not usually needed directly after MultiCell unless you want extra spacing.
 }
 
-func PdfRepport(pdf *fpdf.Fpdf, visit models.Visit) {
-	pdf.AddPage()
+func PdfReport(pdf *fpdf.Fpdf, v models.Visit) {
+	pdf.SetAutoPageBreak(false, 15)
 	pdf.AddUTF8Font("Arial", "", "./static/Arial_Unicode_MS_Regular.ttf")
+	pdf.SetFont("Arial", "", 11)
 
-	// titlen er sagsnr
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, fmt.Sprintf("Sagsnr: %d", visit.Sagsnr))
-	pdf.SetFont("Arial", "", 12)
-	pdf.Ln(12)
+	tpl := gofpdi.ImportPage(pdf, "./static/blanco.pdf", 1, "/MediaBox")
+	pdf.AddPage()
 
-	// og så kommer debitorerne
-	pdf.Cell(float64(len("Debitorer:")*2), 10, "Debitorer: ")
-	for i, debitor := range visit.Debitors {
-		if i == (len(visit.Debitors) - 1) {
-			pdf.Cell(float64(len(debitor.Name))*2, 10, debitor.Name)
-		} else {
-			pdf.Cell(float64(len(debitor.Name+" og "))*2, 10, debitor.Name+" og ")
-		}
+	gofpdi.UseImportedTemplate(pdf, tpl, 0, 0, 210, 0)
+
+	// Now position your fields on top, same as with the image approach
+	write := func(x, y float64, txt string) {
+		pdf.SetXY(x, y)
+		pdf.CellFormat(0, 5, txt, "", 0, "", false, 0, "")
 	}
-	pdf.Ln(10)
 
-	// dato tidspunkt og sted for besøget
-	date := visit.VisitResponse.ActDate.Format("2006-01-02")
-	pdfwrite(pdf, "Besøget var af typen: "+visit.Type.Text)
-	pdfwrite(pdf, "Dato og tidspunkt for besøget: "+visit.VisitResponse.ActTime[:8]+" "+date)
-	pdfwrite(pdf, "Besøget tog sted ved "+visit.Address)
-	// svaret for besøget
-	if visit.VisitResponse.AssetAtAddress {
-		pdfwrite(pdf, "Aktivet var på addressen")
-	} else if visit.VisitResponse.AssetAtWorkshop {
-		pdfwrite(pdf, "Aktivet var på værksted")
+	for i, deb := range v.Debitors {
+		write(30, float64(53+i*5), deb.Name)
+	}
+
+	for i, deb := range v.Debitors {
+		write(100, float64(50+i*5), deb.SSN)
+	}
+
+	if v.VisitResponse.DebitorIsHome {
+		write(20, 80, "X")
 	} else {
-		pdfwrite(pdf, "Aktivets lokation er: "+visit.VisitResponse.AssetLocation)
+		write(20, 90, "X")
 	}
 
-	if visit.VisitResponse.AssetDamaged {
-		pdfwrite(pdf, "Aktivet er skadet")
-	} else {
-		pdfwrite(pdf, "Aktivet er ikke skadet")
+	switch v.VisitResponse.CivilStatus {
+	case models.Cohabiting:
+		write(20, 200, "X")
+	case models.Married:
+		write(20, 230, "X")
+	case models.Single:
+		write(20, 250, "X")
+
 	}
 
-	if visit.VisitResponse.AssetCleaned {
-		pdfwrite(pdf, "Aktivet er rent")
-	} else {
-		pdfwrite(pdf, "Aktivet er ikke rent")
-	}
-
-	if visit.VisitResponse.PaymentReceived {
-		pdfwrite(pdf, "Betaling er modtaget")
-	} else {
-		pdfwrite(pdf, "Betaling er ikke modtaget")
-	}
-	pdfwrite(pdf, "Kommentar til aktivet: "+visit.VisitResponse.AssetComments)
-	pdfwrite(pdf, "Kilometer tal: "+fmt.Sprintf("%d", visit.VisitResponse.OdometerKm))
-
-	if visit.VisitResponse.KeysReceived {
-		pdfwrite(pdf, "nøglerne er modtaget")
-	} else if !visit.VisitResponse.KeysReceived {
-		pdfwrite(pdf, "nøglerne er ikke modtaget")
-	}
-
-	if visit.VisitResponse.DebitorIsHome {
-		if visit.VisitResponse.HasWork {
-			pdfwrite(pdf, "Debitor var hjemme, "+
-				"civilstatus:"+string(visit.VisitResponse.CivilStatus)+
-				". Debitor har  arbejde")
-		} else {
-			pdfwrite(pdf, "Debitor var hjemme, "+
-				"civilstatus:"+string(visit.VisitResponse.CivilStatus)+
-				". Debitor har ikke arbejde")
-		}
-	} else {
-		pdfwrite(pdf, "Debitor var ikke hjemme")
-	}
-
-	// visit.VisitResponse.PropertyType
-
-	pdfwrite(pdf, "Bolig typen er: "+string(visit.VisitResponse.PropertyType))
-	pdfwrite(pdf, "Standen af huset er: "+string(visit.VisitResponse.MaintenanceStatus))
-	pdfwrite(pdf, "Skyldners ejerskabs status er: "+string(visit.VisitResponse.OwnershipStatus))
-
-	pdfwrite(pdf, "Besøgs id: "+fmt.Sprintf("%d", visit.ID))
-	pdfwrite(pdf, "Besøgssvar id: "+fmt.Sprintf("%d", visit.VisitResponse.ID))
-
-	pdfwrite(pdf, "Lat: "+visit.VisitResponse.ActLat+" long: "+visit.VisitResponse.ActLong+" positions sikkerhed: "+visit.VisitResponse.PosAccuracy)
-
-	if visit.VisitResponse.SFSigned {
-		pdfwrite(pdf, "salgsfuldmagt er underskevet")
-	}
-	if visit.VisitResponse.SESigned {
-		pdfwrite(pdf, "salgs-/eftergivelseaftale underskrevet")
-	}
-
-	// børn
-	pdfwrite(pdf, "antal børn under 18: "+fmt.Sprintf("%d", visit.VisitResponse.ChildrenUnder18))
-
-	if visit.VisitResponse.HasWork {
-		pdfwrite(pdf, "skyldner har arbejde")
-		pdfwrite(pdf, "de har stillingen: "+visit.VisitResponse.Position)
-		pdfwrite(pdf, "og skyldner tjener følgende: "+fmt.Sprintf("%f", visit.VisitResponse.Salary))
-
-	} else if !visit.VisitResponse.HasWork {
-		pdfwrite(pdf, "skyldner har ikke arbejde")
-	}
-	pdfwrite(pdf, "skyldner får følgende i pension: "+fmt.Sprintf("%f", visit.VisitResponse.PensionPayment))
-	pdfwrite(pdf, "skyldner får følgende i alt: "+fmt.Sprintf("%f", visit.VisitResponse.IncomePayment))
-	pdfwrite(pdf, "skyldner har følgende a rutte med: "+fmt.Sprintf("%f", visit.VisitResponse.MonthlyDisposableAmount))
-
-	// Write creditor information
-	if visit.VisitResponse.Creditor != "" {
-		pdfwrite(pdf, "Kreditor 1: "+visit.VisitResponse.Creditor)
-		pdfwrite(pdf, "Beløb: "+fmt.Sprintf("%f", visit.VisitResponse.DebtAmount))
-		pdfwrite(pdf, "Aftale: "+visit.VisitResponse.Settlement)
-	}
-
-	if visit.VisitResponse.Creditor2 != "" {
-		pdfwrite(pdf, "Kreditor 2: "+visit.VisitResponse.Creditor2)
-		pdfwrite(pdf, "Beløb: "+fmt.Sprintf("%f", visit.VisitResponse.DebtAmount2))
-		pdfwrite(pdf, "Aftale: "+visit.VisitResponse.Settlement2)
-	}
-
-	if visit.VisitResponse.Creditor3 != "" {
-		pdfwrite(pdf, "Kreditor 3: "+visit.VisitResponse.Creditor3)
-		pdfwrite(pdf, "Beløb: "+fmt.Sprintf("%f", visit.VisitResponse.DebtAmount3))
-		pdfwrite(pdf, "Aftale: "+visit.VisitResponse.Settlement3)
-	}
-
-	pdfwrite(pdf, "kommentarer: "+visit.VisitResponse.Comments)
+	pdf.AddPage()
+	pdfwrite(pdf, "kommentarer: "+v.VisitResponse.Comments)
 
 	// til slut billederne
-	for _, image := range visit.VisitResponse.Images {
+	for _, image := range v.VisitResponse.Images {
 		pdf.AddPage()
 		addImageFit(pdf, image.ImagePath)
 	}
@@ -264,8 +179,8 @@ func GeneratePDFVisit(visitID uint) []byte {
 	pdfBuf := fpdf.New("P", "mm", "A4", "")
 	pdfFile := fpdf.New("P", "mm", "A4", "")
 
-	PdfRepport(pdfBuf, visit)
-	PdfRepport(pdfFile, visit)
+	PdfReport(pdfBuf, visit)
+	PdfReport(pdfFile, visit)
 
 	var buf bytes.Buffer
 	err := pdfBuf.Output(&buf)
